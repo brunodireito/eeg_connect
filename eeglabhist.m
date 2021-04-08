5b9b758d2aff2583af8318c69bee3bcef450ba26
Cristina Melo <a21260726@isec.pt>
2021-04-08 14:53:10 +0100

    Rewriting and editing code to check for errors

--- eeglabhist.m
+++ eeglabhist.m
@@ -1,15 +1,6 @@
 % EEGLAB history file generated on the 08-Apr-2021
 % ------------------------------------------------
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
-
-path_data='C:\\Program Files\\MATLAB\\R2020b\\eeglab2021.0\\sample_data\\';
-
-EEG = pop_loadset('filename','eeglab_data.set','filepath',path_data);
+EEG = pop_loadset('filename','eeglab_data.set','filepath','C:\\Program Files\\MATLAB\\R2020b\\eeglab2021.0\\sample_data\\');
 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
-EEG = eeg_checkset( EEG );
-% EEG = pop_eegfiltnew(EEG, 'locutoff',1,'hicutoff',40,'plotfreqz',1);
-% eeglab redraw;
-
-%% Filtering data between 1Hz and 40Hz
-
-
+eeglab redraw;
--- eeglabhist_01.m
+++ eeglabhist_01.m
@@ -10,6 +10,7 @@ path_data='C:\\Program Files\\MATLAB\\R2020b\\eeglab2021.0\\sample_data\\';
 [ALLEEG EEG CURRENTSET ALLCOM] = eeglab;
 EEG = pop_loadset('filename','eeglab_data.set','filepath',path_data);
 [ALLEEG, EEG, CURRENTSET] = eeg_store( ALLEEG, EEG, 0 );
+eeglab redraw;
 
 
 %% Filtering the data (Between 1 and 40 Hz)
