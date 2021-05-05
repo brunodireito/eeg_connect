ed0b5b122d34cd086776211e95888e656ec8fe5a
Bruno Miguel <migueldireito@gmail.com>
2021-05-05 16:26:35 +0100

    reorganize folder

---
+++ functions/sum2numbers.m
@@ -0,0 +1,8 @@
+function [outputArg1] = sum2numbers(inputArg1,inputArg2)
+%HELLOWORLD Summary of this function goes here
+%   Detailed explanation goes here
+
+outputArg1 = inputArg1 + inputArg2;
+
+end
+
---
+++ main.m
@@ -0,0 +1,26 @@
+%% -- define global variables --
+% Define variables that will be required throughou the project.
+
+% addpath - add important folders to the path
+addpath('functions')
+
+% paths.
+RAWDATA_PATH='';
+RESULTS_PATH='';
+
+% variables.
+% e.g.
+TRI_CORRECT=21;
+TRI_WRONG=20;
+
+
+%% Load data.
+
+% dataset load.
+
+
+%% Data analysis
+% Organize data and call specific functions.
+
+disp(sum2numbers(1,2))
+
